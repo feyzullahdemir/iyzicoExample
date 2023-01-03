a89036bf0c1373c5e8f73f481500198e1d1edc23
feyzullah Demir <101548542+feyzullahdemir@users.noreply.github.com>
2023-01-03 10:52:30 +0300

    Update config.php

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ class Config
     {
         $options = new \Iyzipay\Options();
         $options->setApiKey('sandbox-OInnUVaq5h7rS306Ifa1OTgYdC1bOqyk');
-        $options->setSecretKey('sandbox-JXNnEcyPyqYob8VzL67YhrOIJcPXyjdg');
+        $options->setSecretKey('Is4f0YGq79NCPCdQ1nMrbFbwy1hL1fqo');
         $options->setBaseUrl('https://sandbox-api.iyzipay.com');
         return $options;
     }
